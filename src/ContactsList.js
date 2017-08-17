725398d86d3b0af83a1606db6ec90d159a84c06a
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2017-08-16 17:14:57 -0700

    Filter added to SearchBar to populate list without worrying about lower and uppercase

--- src/ContactsList.js
+++ src/ContactsList.js
@@ -8,26 +8,32 @@ class ContactsList extends React.Component {
 		super();
 
 		this.state = {
-			search: 'Level up'
+			search: ''
 		};
 
 		
 	}
 	updateSearch(event) {
-			this.setState({search: event.target.value.substr(0,10)})
+			this.setState({search: event.target.value.substr(0,20)})
 	}
 
 	render() {
+		let filteredContacts = this.props.contacts.filter(
+				(contact) => {
+					return contact.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+				}
+			);
 		return (
 				<div>
-					<ul>
-						{this.props.contacts.map((cont) => {
-							return <Contact contact={cont} key={cont.id} />
-						})}
-					</ul>
 					<input type="text" 
 					value = {this.state.search}
 					onChange = {this.updateSearch.bind(this)} />
+					<ul>
+						{filteredContacts.map((cont) => {
+							return <Contact contact={cont} key={cont.id} />
+						})}
+					</ul>
+					
 				</div>
 			)
 	}
